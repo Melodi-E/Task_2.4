19728a0b8b4098742c844b7c05651c15723c8b93
Ekaterina Kudyukova <ekudyukova@yandex.ru>
2017-12-04 23:59:43 +0500

    Added file with corrections

--- code_2.4.py
+++ code_2.4.py
@@ -1,26 +1,29 @@
-import glob
-import os.path
+import os
 
 def search_substring_in_file(original_file_list):
 	new_file_list = [] 
 
 	string_to_search = input('Введите строку для поиска:')
-
 	for file_name in original_file_list: 
-		current_file = open(file_name) 
-		file_content = current_file.read() 
-		if string_to_search in file_content: 
-			new_file_list.append(file_name) 
-			print(file_name) 
+		with open(file_name) as current_file:
+			file_content = current_file.read() 
+			if string_to_search in file_content: 
+				new_file_list.append(file_name) 
+				print(file_name)
 	print('Всего: ', len(new_file_list))
 	return new_file_list 
 
 def find_files():
 	migrations = 'Migrations'
+	current_dir = os.path.dirname(os.path.abspath(__file__)) 
+	os.chdir(os.path.join(current_dir, migrations)) 
+
 	file_list = []
-	files = glob.glob(os.path.join(migrations, "*.sql"))
+	files = os.listdir()      
 	for file in files:
-		file_list.append(file)
+		filename, file_extension = os.path.splitext(file)
+		if file_extension == ".sql":
+		    file_list.append(file) 
 	print('Осторожно, бесконечный цикл. Для выхода нажмите Ctrl + C')
 	while True:
 		file_list = search_substring_in_file(file_list)
